a4ddfa57bfbea711e4eb009dc5fb9a3e4ccc7a1d
Maciej Stachowiak <mstachow@src.gnome.org>
2000-06-01 15:28:04 +0000

    Get "can_open_multiple_files" and "can_open_uris" from the right
    	* libgnomevfs/gnome-vfs-mime-handlers.c
    	(gnome_vfs_mime_application_new_from_id): Get
    	"can_open_multiple_files" and "can_open_uris" from the right
    	properties.

--- libgnomevfs/gnome-vfs-mime-handlers.c
+++ libgnomevfs/gnome-vfs-mime-handlers.c
@@ -642,10 +642,10 @@ gnome_vfs_mime_application_new_from_id (const char *id) {
 	}
 
 	application->can_open_multiple_files = str_to_bool (gnome_vfs_mime_get_value (id_hack_mime_type, 
-										      "default_application_name"));
+										      "can_open_multiple_files"));
 	
 	application->can_open_uris = str_to_bool (gnome_vfs_mime_get_value (id_hack_mime_type, 
-									    "default_application_name"));
+									    "can_open_uris"));
 
 	application->id = g_strdup (id);
 
