2971bc47c6494c15567d4a89722873e98eb3da69
Kai Lahmann <kail@src.gnome.org>
2000-12-03 10:32:05 +0000

    Updated German translation

--- po/update.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-xgettext --default-domain=gnome-vfs --directory=.. \
-  --add-comments --keyword=_ --keyword=N_ \
-  --files-from=./POTFILES.in \
-&& test ! -f gnome-vfs.po \
-   || ( rm -f ./gnome-vfs.pot \
-    && mv gnome-vfs.po ./gnome-vfs.pot )
