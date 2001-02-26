008687c804c29284e86f05e891c8f7755b4734fe
Darin Adler <darin@src.gnome.org>
2001-02-26 20:19:44 +0000

    reviewed by: Ramiro Estrugo <ramiro@eazel.com>
    	* check-mime.pl: Added more checks, including a check for
    	unexpected keys, check for repeated keys, check for key format in
    	.mime file, check for icon names mentioned against icon files, and
    	code to make it communicate failure to the shell with "exit 1".

--- check-mime.pl
+++ check-mime.pl
@@ -4,7 +4,7 @@
 #
 #  GNOME VFS
 #
-#  Copyright (C) 2000 Eazel, Inc.
+#  Copyright (C) 2000, 2001 Eazel, Inc.
 #
 #  This library is free software; you can redistribute it and/or
 #  modify it under the terms of the GNU General Public License as
@@ -32,6 +32,7 @@
 #     types in gnome-vfs.mime have descriptions in gnome-vfs.keys.in
 #     types in gnome-vfs-mime-magic have descriptions in gnome-vfs.keys.in
 #     types in gnome-vfs.applications have descriptions in gnome-vfs.keys.in
+#     (need to add what we check now)
 
 # Other things to check later:
 #     OAFIIDs are consistent (same UUID part for same prefix)
@@ -42,12 +43,53 @@ use strict;
 
 my %seen;
 
-print STDERR "Reading gnome-vfs.keys.in.\n";
-
 my $previous_type = "";
 my $type;
 my %in_keys;
 my %described;
+
+my %seen_keys;
+
+my @expected_keys;
+my %expected_keys;
+
+my $exit_status = 0;
+
+sub complain ($)
+  {
+    my ($complaint) = @_;
+
+    print STDERR "- ", $complaint, "\n";
+    $exit_status = 1;
+  }
+
+my $check_icons = -d "../nautilus/icons";
+if ($check_icons)
+  {
+    print "Found Nautilus icon directory, checking icons.\n";
+  }
+else
+  {
+    print "Unable to locate Nautilus icon directory, not checking icons.\n";
+  }
+              
+print "Reading gnome-vfs.keys.in.\n";
+
+@expected_keys =
+  (
+   "can_be_executable",
+   "default_action_type",
+   "icon_filename",
+   "open",
+   "short_list_application_ids_for_advanced_user_level",
+   "short_list_application_ids_for_intermediate_user_level",
+   "short_list_application_ids_for_novice_user_level",
+   "short_list_component_iids_for_advanced_user_level",
+   "short_list_component_iids_for_intermediate_user_level",
+   "short_list_component_iids_for_novice_user_level",
+   "vfs_method",
+  );
+@expected_keys{@expected_keys} = (1) x @expected_keys;
 
 open KEYS, "data/mime/gnome-vfs.keys.in" or die;
 while (<KEYS>)
@@ -55,35 +97,35 @@ while (<KEYS>)
     chomp;
     if (/^\s+$/)
       {
-        print "- blank line with spaces on line $.\n";
+        complain "blank line with spaces on line $.";
       }
     else
       {
         if (/^ /)
           {
-            print "- leading spaces used instead of a tab on line $.\n";
+            complain "leading spaces used instead of a tab on line $.";
           }
         if (/\s$/)
           {
-            print "- trailing whitespace on line $.\n";
+            complain "trailing whitespace on line $.";
           }
         if (/\t /)
           {
-            print "- space after tab on line $.\n";
+            complain "space after tab on line $.";
           }
         if (/  /)
           {
-            print "- multiple spaces on line $.\n";
+            complain "multiple spaces on line $.";
           }
       }
     next if /^\s*\#/;
     if (/\s=/)
       {
-        print "- space before = on line $.\n";
+        complain "space before = on line $.";
       }
     if (/=\s/)
       {
-        print "- space after = on line $.\n";
+        complain "space after = on line $.";
       }
     if (/^(\S.*)/)
       {
@@ -91,43 +133,81 @@ while (<KEYS>)
 	$type = $1;
 	if ($type ne lc $type && !$seen{$type})
 	  {
-	    print "- $type contains upper-case letters\n";
+	    complain "$type contains upper-case letters";
 	  }
 	if (lc $type le lc $previous_type)
 	  {
-	    print "- $type is after $previous_type\n";
+	    complain "$type is after $previous_type";
 	  }
 	$seen{$type} = 1;
 	$in_keys{$type} = 1;
-      }
-    elsif (/^\s*_description=/)
-      {
-	if ($type eq "")
-	  {
-	    print "- description outside any type at line $.\n";
-	  }
-	$described{$type} = 1;
-      }
-    elsif (/^\s*icon_filename\s*=\s*i-regular/)
-      {
-        if ($type eq "")
-          {
-            print "- i-regular specified explicitly at line $.\n";
-          }
-        else
-          {
-            print "- i-regular specified for $type at line $.\n";
-          }
+
+        %seen_keys = ();
       }
     elsif (/^\s*$/)
       {
 	$previous_type = $type if $type;
 	$type = "";
       }
+    elsif (/^\s*([_a-z]+)\s*=\s*(.*)/)
+      {
+        if ($seen_keys{$1})
+          {
+            complain "saw key $1 twice at line $.";
+          }
+        else
+          {
+            $seen_keys{$1} = 1;
+          }
+        if ($1 eq "_description")
+          {
+            if ($type eq "")
+              {
+                complain "description outside any type at line $.";
+              }
+            $described{$type} = 1;
+          }
+        elsif ($1 eq "icon_filename")
+          {
+            if ($2 eq "i-regular")
+              {
+                if ($type eq "")
+                  {
+                    complain "i-regular specified explicitly at line $.";
+                  }
+                else
+                  {
+                    complain "i-regular specified for $type at line $.";
+                  }
+              }
+            elsif ($check_icons)
+              {
+                if (not -f "../nautilus/icons/$2" and not -f "../nautilus/icons/$2.png")
+                  {
+                    complain "$2 specified for $type, but not in nautilus/icons directory";
+                  }
+              }
+          }
+        elsif (!$expected_keys{$1})
+          {
+            if ($type ne "application/mime-type-test")
+              {
+                complain "saw unexpected key $1 at line $.";
+              }
+          }
+      }
+    elsif (/^\s*(.+?)\s*=/)
+      {
+        complain "saw bad characters in key $1 at line $.";
+      }
+    else
+      {
+        complain "could not parse line $.";
+      }
   }
 close KEYS;
 
-print STDERR "Reading gnome-vfs-mime-magic.\n";
+print "Reading gnome-vfs-mime-magic.\n";
 
 my %in_magic;
 
@@ -137,21 +217,21 @@ while (<MAGIC>)
     chomp;
     if (/^\s+$/)
       {
-        print "- blank line with spaces on line $.\n";
+        complain "blank line with spaces on line $.";
       }
     else
       {
         if (/^ /)
           {
-            print "- leading spaces used instead of a tab on line $.\n";
+            complain "leading spaces used instead of a tab on line $.";
           }
         if (/\s$/)
           {
-            print "- trailing whitespace on line $.\n";
+            complain "trailing whitespace on line $.";
           }
         if (/\t /)
           {
-            print "- space after tab on line $.\n";
+            complain "space after tab on line $.";
           }
       }
     next if /^\s*\#/;
@@ -161,17 +241,17 @@ while (<MAGIC>)
 	$type = $3;
 	if ($type ne lc $type && !$seen{$type})
 	  {
-	    print "- $type contains upper-case letters\n";
+	    complain "$type contains upper-case letters";
 	  }
 	if (!$described{$type} && !$in_magic{$type})
 	  {
 	    if (!$in_keys{$type})
 	      {
-		print "- $type is in gnome-vfs-mime-magic, but not gnome-vfs.keys.in\n";
+		complain "$type is in gnome-vfs-mime-magic, but not gnome-vfs.keys.in";
 	      }
 	    else
 	      {
-		print "- $type is in gnome-vfs-mime-magic, but has no description in gnome-vfs.keys.in\n";
+		complain "$type is in gnome-vfs-mime-magic, but has no description in gnome-vfs.keys.in";
 	      }
 	  }
 	$seen{$type} = 1;
@@ -179,12 +259,19 @@ while (<MAGIC>)
       }
     else
       {
-	print "Could not parse line in gnome-vfs-mime-magic:\n$_";
+	complain "could not parse line $.";
       }
   }
 close MAGIC;
 
-print STDERR "Reading gnome-vfs.mime.\n";
+print "Reading gnome-vfs.mime.\n";
+
+@expected_keys =
+  (
+   "ext",
+   "regex",
+  );
+@expected_keys{@expected_keys} = (1) x @expected_keys;
 
 $previous_type = "";
 $type = "";
@@ -196,25 +283,25 @@ while (<MIME>)
     chomp;
     if (/^\s+$/)
       {
-        print "- blank line with spaces on line $.\n";
+        complain "blank line with spaces on line $.";
       }
     else
       {
         if (/^ /)
           {
-            print "- leading spaces used instead of a tab on line $.\n";
+            complain "leading spaces used instead of a tab on line $.";
           }
         if (/\s$/)
           {
-            print "- trailing whitespace on line $.\n";
+            complain "trailing whitespace on line $.";
           }
         if (/\t /)
           {
-            print "- space after tab on line $.\n";
+            complain "space after tab on line $.";
           }
         if (/  /)
           {
-            print "- multiple spaces on line $.\n";
+            complain "multiple spaces on line $.";
           }
       }
     next if /^\s*\#/;
@@ -224,21 +311,21 @@ while (<MIME>)
 	$type = $1;
 	if ($type ne lc $type && !$seen{$type})
 	  {
-	    print "- $type contains upper-case letters\n";
+	    complain "$type contains upper-case letters";
 	  }
 	if (lc $type le lc $previous_type)
 	  {
-	    print "- $type is after $previous_type in gnome-vfs.mime\n";
+	    complain "$type is after $previous_type in gnome-vfs.mime";
 	  }
 	if (!$described{$type} && !$in_mime{$type})
 	  {
 	    if (!$in_keys{$type})
 	      {
-		print "- $type is in gnome-vfs.mime, but not gnome-vfs.keys.in\n";
+		complain "$type is in gnome-vfs.mime, but not gnome-vfs.keys.in";
 	      }
 	    else
 	      {
-		print "- $type is in gnome-vfs.mime, but has no description in gnome-vfs.keys.in\n";
+		complain "$type is in gnome-vfs.mime, but has no description in gnome-vfs.keys.in";
 	      }
 	  }
 	$seen{$type} = 1;
@@ -249,16 +336,36 @@ while (<MIME>)
 	$previous_type = $type if $type;
 	$type = "";
       }
+    elsif (/^\s+([a-z]+)(,\d+)?:/)
+      {
+        if (!$expected_keys{$1})
+          {
+            complain "saw unexpected key $1 at line $.";
+          }
+      }
+    else
+      {
+        complain "could not parse line $.";
+      }
   }
 close MIME;
 
-print STDERR "Reading gnome-vfs.applications.\n";
+print "Reading gnome-vfs.applications.\n";
 
-my %in_applications;
+@expected_keys =
+  (
+   "can_open_multiple_files",
+   "command",
+   "expects_uris",
+   "name",
+   "requires_terminal",
+   "supported_uri_schemes",
+  );
+@expected_keys{@expected_keys} = (1) x @expected_keys;
 
 # special case for text/* for now
 # could put in fancier handling later
-$in_applications{"text/*"} = 1;
+my %in_applications = ( "text/*" => 1 );
 
 open MIME, "data/mime/gnome-vfs.applications" or die;
 while (<MIME>)
@@ -266,59 +373,89 @@ while (<MIME>)
     chomp;
     if (/^\s+$/)
       {
-        print "- blank line with spaces on line $.\n";
+        complain "blank line with spaces on line $.";
       }
     else
       {
         if (/^ /)
           {
-            print "- leading spaces used instead of a tab on line $.\n";
+            complain "leading spaces used instead of a tab on line $.";
           }
         if (/\s$/)
           {
-            print "- trailing whitespace on line $.\n";
+            complain "trailing whitespace on line $.";
           }
         if (/\t /)
           {
-            print "- space after tab on line $.\n";
+            complain "space after tab on line $.";
           }
         if (/  /)
           {
-            print "- multiple spaces on line $.\n";
+            complain "multiple spaces on line $.";
           }
       }
     if (/\s=/)
       {
-        print "- space before = on line $.\n";
+        complain "space before = on line $.";
       }
     if (/=\s/)
       {
-        print "- space after = on line $.\n";
+        complain "space after = on line $.";
       }
-    if (/^\s+mime_types=(.*)/)
-      {
-        foreach my $type (split ",", $1)
-          {
-            next if $in_applications{$type};
 
-            if ($type ne lc $type && !$seen{$type})
-              {
-                print "- $type contains upper-case letters\n";
-              }
-            if (!$described{$type} && !$in_applications{$type})
-              {
-                if (!$in_keys{$type})
-                  {
-                    print "- $type is in gnome-vfs.applications, but not gnome-vfs.keys.in\n";
-                  }
-                else
-                  {
-                    print "- $type is in gnome-vfs.applications, but has no description in gnome-vfs.keys.in\n";
-                  }
-              }
-            $seen{$type} = 1;
-            $in_applications{$type} = 1;
+    if (/^\S/)
+      {
+        %seen_keys = ();
+      }
+    elsif (/^\s*([_a-z]+)\s*=\s*(.*)/)
+      {
+        if ($seen_keys{$1})
+          {
+            complain "saw key $1 twice at line $.";
           }
+        else
+          {
+            $seen_keys{$1} = 1;
+          }
+        if ($1 eq "mime_types")
+          {
+            foreach my $type (split ",", $2)
+              {
+                next if $in_applications{$type};
+                
+                if ($type ne lc $type && !$seen{$type})
+                  {
+                    complain "$type contains upper-case letters";
+                  }
+                if (!$described{$type} && !$in_applications{$type})
+                  {
+                    if (!$in_keys{$type})
+                      {
+                        complain "$type is in gnome-vfs.applications, but not gnome-vfs.keys.in";
+                      }
+                    else
+                      {
+                        complain "$type is in gnome-vfs.applications, but has no description in gnome-vfs.keys.in";
+                      }
+                  }
+                $seen{$type} = 1;
+                $in_applications{$type} = 1;
+              }
+          }
+        elsif (!$expected_keys{$1})
+          {
+            complain "saw unexpected key $1 at line $.";
+          }
+      }
+    elsif (/^\s*(.+?)\s*=/)
+      {
+        complain "saw bad characters in key $1 at line $.";
+      }
+    elsif (!/^\s*$/)
+      {
+        complain "could not parse line $.";
       }
   }
 close MIME;
+
+exit $exit_status;
