3818c3e3377e5d864ab723d9d9d50a2f920fb6a7
George Lebl <jirka@5z.com>
2001-10-23 02:07:02 +0000

    add includes for sys/time and sys/types
    Mon Oct 22 19:10:57 2001  George Lebl <jirka@5z.com>
    
    	* modules/test-method.c: add includes for sys/time and sys/types

--- modules/test-method.c
+++ modules/test-method.c
@@ -47,6 +47,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/time.h>
+#include <sys/types.h>
 #include <unistd.h>
 
 #define TEST_CONF_ENV_VARIABLE "GNOME_VFS_TEST_CONFIG_FILE"
