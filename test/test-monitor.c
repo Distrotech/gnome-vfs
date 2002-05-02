0a8ab4d44bc3fe3b96325ce243e595857a8db04f
Bastien Nocera <hadess@src.gnome.org>
2002-05-02 18:37:06 +0000

    - make the test be automatic, by exiting after 2 events, add in an timeout function 1 file create and 1 file deletion

--- test/test-monitor.c
+++ test/test-monitor.c
@@ -32,6 +32,28 @@
 
 static GMainLoop *main_loop;
 
+static gboolean
+timeout_cb (gpointer data)
+{
+	static int i = 0;
+
+	if (i == 0)
+	{
+		unlink ("/tmp/test-monitor");
+		g_spawn_command_line_sync ("touch /tmp/test-monitor",
+				NULL, NULL, NULL, NULL);
+	}
+
+	if (i == 1)
+	{
+		unlink ("/tmp/test-monitor");
+	}
+
+	i++;
+
+	return TRUE;
+}
+
 static void
 show_result (GnomeVFSResult result, const gchar *what, const gchar *text_uri)
 {
@@ -47,6 +69,8 @@ callback (GnomeVFSMonitorHandle *handle,
 	  const gchar *info_uri,
 	  GnomeVFSMonitorEventType event_type,
 	  gpointer user_data) {
+	static int i = 0;
+
 	g_print ("Got a callback: ");
 	switch (event_type) {
 		case GNOME_VFS_MONITOR_EVENT_CHANGED:
@@ -67,10 +91,16 @@ callback (GnomeVFSMonitorHandle *handle,
 		case GNOME_VFS_MONITOR_EVENT_METADATA_CHANGED:
 			g_print ("GNOME_VFS_MONITOR_EVENT_METADATA_CHANGED");
 			break;
+		default:
+			g_print ("Unknown monitor type, exiting...");
+			exit (1);
 	}
 
 	g_print (" (%s)", info_uri);
 	g_print ("\n");
+	i++;
+	if (i >= 2)
+		exit (0);
 }
 
 int
@@ -93,6 +123,8 @@ main (int argc, char **argv)
 			GNOME_VFS_MONITOR_DIRECTORY, callback, "user data");
 	printf ("handle is %p\n", handle);
 	show_result (result, "monitor_add", text_uri);
+
+	g_timeout_add (1000, timeout_cb, NULL);
 
 	if (result == GNOME_VFS_OK) {
 		main_loop = g_main_loop_new (NULL, TRUE);
