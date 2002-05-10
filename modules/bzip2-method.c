ebb1038b641acd7be32e047f5ee21f545eebe4e1
Cody Russell <bratsche@src.gnome.org>
2002-05-10 03:38:36 +0000

    Updating contact info.

--- modules/bzip2-method.c
+++ modules/bzip2-method.c
@@ -19,7 +19,7 @@
  * write to the Free Software Foundation, Inc., 59 Temple Place - Suite
  * 330, Boston, MA 02111-1307, USA.
  *
- * Author: Cody Russell <bratsche@dfw.net>
+ * Author: Cody Russell  <cody@jhu.edu>
  */
 
 #include <config.h>
