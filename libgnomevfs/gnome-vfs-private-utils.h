9c9b25922a04e0a53c341d777eabc8445c795c8e
Christophe Fergeau <teuf@users.sourceforge.net>
2003-04-18 09:30:37 +0000

    removed misleading comment modules/cdemenu-module.conf removed .so
    2003-04-18  Christophe Fergeau  <teuf@users.sourceforge.net>
    
    	* libgnomevfs/gnome-vfs-private-utils.h: removed misleading comment
    	* modules/cdemenu-module.conf
    	* modules/default-modules.conf: removed .so extension to module names
    	  since this is added automatically by gmodule, and it breaks on
    	  some unices (eg HPUX). Fixes #110986

--- libgnomevfs/gnome-vfs-private-utils.h
+++ libgnomevfs/gnome-vfs-private-utils.h
@@ -59,7 +59,6 @@ GnomeVFSResult	gnome_vfs_create_temp 	(const gchar *prefix,
 gboolean	gnome_vfs_atotm		(const gchar *time_string,
 					 time_t *value_return);
 
-/* Wrapper around gnome_i18n_get_language_list(). */
 GList	       *gnome_vfs_i18n_get_language_list
 					(const gchar *category_name);
 
