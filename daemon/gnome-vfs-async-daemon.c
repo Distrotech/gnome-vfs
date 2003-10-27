2dc3c44f88c697f2ac8e95e1d5545999a5dce335
Alexander Larsson <alexl@redhat.com>
2003-10-27 10:42:57 +0000

    FreeBSD has a public async_daemon symbol. Avoid collision. Patch from Joe
    2003-10-27  Alexander Larsson  <alexl@redhat.com>
    
    	* daemon/gnome-vfs-async-daemon.c:
    	FreeBSD has a public async_daemon symbol. Avoid collision.
    	Patch from Joe Marcus Clarke <marcus@FreeBSD.org>

--- daemon/gnome-vfs-async-daemon.c
+++ daemon/gnome-vfs-async-daemon.c
@@ -23,23 +23,23 @@ BONOBO_CLASS_BOILERPLATE_FULL(
  *  the context object that has been looked up */
 G_LOCK_DEFINE_STATIC (client_call_context);
 
-static GnomeVFSAsyncDaemon *async_daemon = NULL;
+static GnomeVFSAsyncDaemon *g_vfs_async_daemon = NULL;
 
 static void
 gnome_vfs_async_daemon_finalize (GObject *object)
 {
 	/* All client calls should have finished before we kill this object */
-	g_assert (g_hash_table_size (async_daemon->client_call_context) == 0);
-	g_hash_table_destroy (async_daemon->client_call_context);
+	g_assert (g_hash_table_size (g_vfs_async_daemon->client_call_context) == 0);
+	g_hash_table_destroy (g_vfs_async_daemon->client_call_context);
 	BONOBO_CALL_PARENT (G_OBJECT_CLASS, finalize, (object));
-	async_daemon = NULL;
+	g_vfs_async_daemon = NULL;
 }
 
 static void
 gnome_vfs_async_daemon_instance_init (GnomeVFSAsyncDaemon *daemon)
 {
 	daemon->client_call_context = g_hash_table_new (NULL, NULL);
-	async_daemon = daemon;
+	g_vfs_async_daemon = daemon;
 }
 
 GnomeVFSContext *
@@ -48,13 +48,13 @@ gnome_vfs_async_daemon_get_context (const GNOME_VFS_ClientCall client_call,
 {
 	GnomeVFSContext *context;
 
-	if (async_daemon == NULL) {
+	if (g_vfs_async_daemon == NULL) {
 		return NULL;
 	}
 	
 	context = gnome_vfs_context_new ();
 	G_LOCK (client_call_context);
-	g_hash_table_insert (async_daemon->client_call_context, client_call, context);
+	g_hash_table_insert (g_vfs_async_daemon->client_call_context, client_call, context);
 	G_UNLOCK (client_call_context);
 
 	gnome_vfs_daemon_add_context (client, context);
@@ -72,8 +72,8 @@ gnome_vfs_async_daemon_drop_context (const GNOME_VFS_ClientCall client_call,
 		_gnome_vfs_daemon_set_current_daemon_client_call (NULL);
 		gnome_vfs_daemon_remove_context (client, context);
 		G_LOCK (client_call_context);
-		if (async_daemon != NULL) {
-			g_hash_table_remove (async_daemon->client_call_context, client_call);
+		if (g_vfs_async_daemon != NULL) {
+			g_hash_table_remove (g_vfs_async_daemon->client_call_context, client_call);
 		}
 		gnome_vfs_context_free (context);
 		G_UNLOCK (client_call_context);
@@ -176,7 +176,7 @@ cancel_client_call_callback (gpointer data)
 	client_call = data;
 
 	G_LOCK (client_call_context);
-	context = g_hash_table_lookup (async_daemon->client_call_context, client_call);
+	context = g_hash_table_lookup (g_vfs_async_daemon->client_call_context, client_call);
 	if (context != NULL) {
 		cancellation = gnome_vfs_context_get_cancellation (context);
 		if (cancellation) {
