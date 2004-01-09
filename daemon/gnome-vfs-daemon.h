33a553f21a0671505e29552b0dfc722bee039ca3
Alexander Larsson <alexl@redhat.com>
2004-01-09 13:25:08 +0000

    Add GPL headers to vfs-daemon code.
    2004-01-09  Alexander Larsson  <alexl@redhat.com>
    
    	* daemon/gnome-vfs-async-daemon.c:
    	* daemon/gnome-vfs-async-daemon.h:
    	* daemon/gnome-vfs-daemon-dir-handle.c:
    	* daemon/gnome-vfs-daemon-dir-handle.h:
    	* daemon/gnome-vfs-daemon-handle.c:
    	* daemon/gnome-vfs-daemon-handle.h:
    	* daemon/gnome-vfs-daemon.c:
    	* daemon/gnome-vfs-daemon.h:
    	Add GPL headers to vfs-daemon code.

--- daemon/gnome-vfs-daemon.h
+++ daemon/gnome-vfs-daemon.h
@@ -1,3 +1,23 @@
+/*
+ *  Copyright (C) 2003, 2004 Red Hat, Inc.
+ *
+ *  Nautilus is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License as
+ *  published by the Free Software Foundation; either version 2 of the
+ *  License, or (at your option) any later version.
+ *
+ *  Nautilus is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ *  Authors: Alexander Larsson <alexl@redhat.com>
+ *
+ */
 #ifndef _GNOME_VFS_DAEMON_H_
 #define _GNOME_VFS_DAEMON_H_
 
