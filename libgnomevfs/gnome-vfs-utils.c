21e3c6ff3f986a3a6ebe21162e71e766732a256b
Christian Neumair <chris@gnome-de.org>
2005-10-17 18:14:50 +0000

    Correctly resolve nested symlinks without accessing invalid memory. Fixes
    2005-10-17  Christian Neumair  <chris@gnome-de.org>
    
    	* libgnomevfs/gnome-vfs-utils.c:
    	(_gnome_vfs_uri_resolve_all_symlinks_uri):
    	Correctly resolve nested symlinks without accessing invalid memory.
    	Fixes #318457.

--- libgnomevfs/gnome-vfs-utils.c
+++ libgnomevfs/gnome-vfs-utils.c
@@ -1991,9 +1991,14 @@ _gnome_vfs_uri_resolve_all_symlinks_uri (GnomeVFSURI *uri,
 			resolved_uri = gnome_vfs_uri_resolve_relative (new_uri,
 								       info->symlink_name);
 			if (*p != 0) {
+				gnome_vfs_uri_unref (new_uri);
+
+				new_uri = gnome_vfs_uri_append_string (resolved_uri, p);
+
 				gnome_vfs_uri_unref (uri);
-				uri = gnome_vfs_uri_append_string (resolved_uri, p);
 				gnome_vfs_uri_unref (resolved_uri);
+
+				uri = gnome_vfs_uri_ref (new_uri);
 			} else {
 				gnome_vfs_uri_unref (uri);
 				uri = resolved_uri;
