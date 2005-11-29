3526a576b2d01211a4302fa7c34ef3c6bc614489
John McCutchan <jmc@src.gnome.org>
2005-11-29 03:15:54 +0000

    Compile inotify-kernel.c, ship inotify-kernel.h.
    * modules/Makefile.am: Compile inotify-kernel.c, ship inotify-kernel.h.
    * modules/inotify-kernel.c: (ik_startup), (ik_event_internal_new),
    (ik_event_new), (ik_event_free), (ik_watch), (ik_ignore),
    (ik_read_events), (ik_read_callback), (g_timeval_lt),
    (g_timeval_eq), (ik_pair_events), (ik_event_add_microseconds),
    (ik_event_ready), (ik_pair_moves), (ik_process_events),
    (ik_process_eq_callback):
    * modules/inotify-kernel.h: Factored out inotify code that deals with the
    kernel and raw events.

---
+++ modules/inotify-kernel.h
@@ -0,0 +1,39 @@
+/*
+	Copyright (C) 2005 John McCutchan
+
+	This program is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; version 2.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License version 2 for more details.
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software Foundation,
+	Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+*/
+
+#ifndef __INOTIFY_KERNEL_H
+#define __INOTIFY_KERNEL_H
+
+typedef struct {
+    guint32 wd;
+    guint32 mask;
+    guint32 cookie;
+    guint32 len;
+    char *  name;
+	gboolean paired;
+	guint32 pair_wd;
+	guint32 pair_mask;
+	guint32 pair_len;
+	char *  pair_name;
+} ik_event_t;
+
+gboolean ik_startup (void (*cb)(ik_event_t *event));
+void ik_event_free (ik_event_t *event);
+
+guint32 ik_watch(const char *path, guint32 mask, int *err);
+int ik_ignore(guint32 wd);
+
+#endif
