008bd051120aa11fb562715f52c27c4aaddb6232
Christian Kellner <gicmo@gnome.org>
2006-01-10 15:06:45 +0000

    Require glib 2.9.2
    2006-01-10  Christian Kellner  <gicmo@gnome.org>
    
    	* configure.in: Require glib 2.9.2
    
    	* modules/file-method.c: Remove code copied from glib obsoleted
    	with the dependency to glib 2.9.2.

--- modules/file-method.c
+++ modules/file-method.c
@@ -84,63 +84,6 @@
 #define DIR_SEPARATORS "/"
 #endif
 
-#if !GLIB_CHECK_VERSION (2,7,0)
-/* g_access lifted from GLib */
-#ifndef G_OS_WIN32
-#define g_access(filename, mode) access (filename, mode)
-#else
-static int
-g_access (const gchar *filename,
-	  int          mode)
-{
-#ifdef G_OS_WIN32
-  if (G_WIN32_HAVE_WIDECHAR_API ())
-    {
-      wchar_t *wfilename = g_utf8_to_utf16 (filename, -1, NULL, NULL, NULL);
-      int retval;
-      int save_errno;
-      
-      if (wfilename == NULL)
-	{
-	  errno = EINVAL;
-	  return -1;
-	}
-
-      retval = _waccess (wfilename, mode);
-      save_errno = errno;
-
-      g_free (wfilename);
-
-      errno = save_errno;
-      return retval;
-    }
-  else
-    {    
-      gchar *cp_filename = g_locale_from_utf8 (filename, -1, NULL, NULL, NULL);
-      int retval;
-      int save_errno;
-
-      if (cp_filename == NULL)
-	{
-	  errno = EINVAL;
-	  return -1;
-	}
-
-      retval = access (cp_filename, mode);
-      save_errno = errno;
-
-      g_free (cp_filename);
-
-      errno = save_errno;
-      return retval;
-    }
-#else
-  return access (filename, mode);
-#endif
-}
-#endif
-#endif
-
 #ifdef HAVE_FAM
 static FAMConnection *fam_connection = NULL;
 static gint fam_watch_id = 0;
